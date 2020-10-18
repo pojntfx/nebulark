16696bd0a2afbb21b5fb515c8d25947336a108f2
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-18 15:04:43 +0000

    feat: Cross-compile Jansson, add basic JSON unmarshaller for C WASM

--- pkg/sparkexamples/c/json_calculator/calculator.c
+++ pkg/sparkexamples/c/json_calculator/calculator.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #include "_deps/base64/base64.h"
-#include "build/include/jansson.h"
+#include "jansson.h"
 
 static unsigned char *decode(char *decode, unsigned int decodelen) {
   unsigned char *decode_out;
@@ -31,10 +31,47 @@ static char *encode(unsigned char *encode, unsigned int encodelen) {
   return encode_out;
 }
 
+typedef struct {
+  int first_addend;
+  int second_addend;
+} addends_t;
+
+static int unmarshal_addends(addends_t *addends, char *data) {
+  json_t *root;
+  json_error_t json_error;
+
+  root = json_loads(data, 0, &json_error);
+
+  int first_addend;
+  int second_addend;
+
+  json_unpack(root, "{s:i, s:i}", "firstAddend", &first_addend, "secondAddend",
+              &second_addend);
+  if (!root) {
+    return 1;
+  }
+
+  addends->first_addend = first_addend;
+  addends->second_addend = second_addend;
+
+  return 0;
+}
+
 int main(void) {
   printf("Decoded: %s\n", decode("Zm9vYmFy", 8));
 
   printf("Encoded: %s\n", encode((void *)"foobar", 6));
+
+  addends_t addends = {1, 1};
+
+  int err =
+      unmarshal_addends(&addends, "{\"firstAddend\": 5, \"secondAddend\": 2}");
+  if (err != 0) {
+    return 1;
+  }
+
+  printf("firstAddend: %d, secondAddend: %d\n", addends.first_addend,
+         addends.second_addend);
 
   return 0;
 }
