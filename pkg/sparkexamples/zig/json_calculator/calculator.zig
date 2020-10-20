8d5d04700eca0ad3c791eaea655e8cce1c97f3f0
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-20 23:44:31 +0000

    feat: Add JSON unmarshalling and base64 encode support to Zig JSON calculator

--- pkg/sparkexamples/zig/json_calculator/calculator.zig
+++ pkg/sparkexamples/zig/json_calculator/calculator.zig
@@ -1,3 +1,15 @@
+comptime {
+    @export(construct, .{ .name = "nebulark_ion_spark_construct", .linkage = .Strong });
+    @export(inputSetLength, .{ .name = "nebulark_ion_spark_input_set_length", .linkage = .Strong });
+    @export(inputSet, .{ .name = "nebulark_ion_spark_input_set", .linkage = .Strong });
+    @export(open, .{ .name = "nebulark_ion_spark_open", .linkage = .Strong });
+    @export(ignite, .{ .name = "nebulark_ion_spark_ignite", .linkage = .Strong });
+    @export(close, .{ .name = "nebulark_ion_spark_close", .linkage = .Strong });
+    @export(outputGetLength, .{ .name = "nebulark_ion_spark_output_get_length", .linkage = .Strong });
+    @export(outputGet, .{ .name = "nebulark_ion_spark_output_get", .linkage = .Strong });
+    @export(deconstruct, .{ .name = "nebulark_ion_spark_deconstruct", .linkage = .Strong });
+}
+
 const std = @import("std");
 
 const Input = struct {
@@ -17,11 +29,11 @@ var second_addend: i64 = 0;
 
 var sum: i64 = 0;
 
-export fn nebulark_ion_spark_construct() i32 {
+export fn construct() i32 {
     return 0;
 }
 
-export fn nebulark_ion_spark_input_set_length(length: i32) i32 {
+export fn inputSetLength(length: i32) i32 {
     input_encoded = std.ArrayList(u8).init(std.heap.page_allocator);
 
     input_encoded.appendNTimes(0, @intCast(usize, length)) catch |err| {
@@ -31,19 +43,18 @@ export fn nebulark_ion_spark_input_set_length(length: i32) i32 {
     return 0;
 }
 
-export fn nebulark_ion_spark_input_set(index: i32 , input: u8 ) i32 {
+export fn inputSet(index: i32 , input: u8 ) i32 {
     input_encoded.items[@intCast(usize, index)] = input;
 
     return 0;
 }
 
-export fn nebulark_ion_spark_open() i32 {
+export fn open() i32 {
     const right_bound = std.base64.standard_decoder.calcSize(input_encoded.items) catch |err| {
         return 1;
     };
-    var inputBuffer: [0x100]u8 = undefined;
-    var decoded_input = inputBuffer[0..right_bound];
-
+    var buffer: [0x100]u8 = undefined;
+    var decoded_input = buffer[0..right_bound];
     std.base64.standard_decoder.decode(decoded_input, input_encoded.items) catch |err| {
         return 1;
     };
@@ -64,24 +75,45 @@ export fn nebulark_ion_spark_open() i32 {
     return 0;
 }
 
-export fn nebulark_ion_spark_ignite() i32 {
+export fn ignite() i32 {
     sum = first_addend + second_addend;
 
     return 0;
 }
 
-export fn nebulark_ion_spark_close() i32 {
+export fn close() i32 {
+    const output = Output{
+        .sum = sum
+    };
+
+    var jsonBuffer: [0x100]u8 = undefined;
+    var decoded_output = std.io.fixedBufferStream(&jsonBuffer);
+    std.json.stringify(output, std.json.StringifyOptions{}, decoded_output.outStream()) catch |err| {
+        return 1;
+    };
+
+    var base64Buffer: [0x100]u8 = undefined;
+    var output_encoded_internal = base64Buffer[0..std.base64.Base64Encoder.calcSize(decoded_output.getWritten().len)];
+    std.base64.standard_encoder.encode(output_encoded_internal, decoded_output.getWritten());
+
+    output_encoded = std.ArrayList(u8).init(std.heap.page_allocator);
+    for (output_encoded_internal) |character| {
+        output_encoded.append(character) catch |err| {
+            return 1;
+        };
+    }
+    
     return 0;
 }
 
-export fn nebulark_ion_spark_output_get_length() i32 {
-    return 0;
+export fn outputGetLength() i32 {
+    return @intCast(i32, output_encoded.items.len);
 }
 
-export fn nebulark_ion_spark_output_get(index: i32) u8 {
-    return 0;
+export fn outputGet(index: i32) u8 {
+    return output_encoded.items[@intCast(usize, index)];
 }
 
-export fn nebulark_ion_spark_deconstruct() i32 {
+export fn deconstruct() i32 {
     return 0;
 }
