4f7e011bd2626c7134aeb321e5424a43dc665f32
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-20 00:38:08 +0000

    refactor: Make TinyGo glue code WASI-compatible

--- web/glue/registerFunctions.js
+++ web/glue/registerFunctions.js
@@ -60,6 +60,41 @@ global.openWASIWASMModule = (url, handler) =>
       )
   );
 
+global.openTinyGoWASMModule = (url, wasmRuntimeURL, handler) =>
+  import(wasmRuntimeURL).then((wasmImports) =>
+    import("https://unpkg.com/@wasmer/wasi@0.12.0/lib/index.esm.js").then(
+      (wasiModule) =>
+        import("https://unpkg.com/@wasmer/wasmfs@0.12.0/lib/index.esm.js").then(
+          (wasmFsModule) =>
+            fetch(url)
+              .then((resp) => resp.arrayBuffer())
+              .then((res) => new Uint8Array(res).buffer)
+              .then(WebAssembly.compile)
+              .then((bytes) => {
+                const wasmFs = new wasmFsModule.WasmFs();
+
+                const wasi = new wasiModule.WASI({
+                  args: [],
+                  env: {},
+                  bindings: {
+                    ...wasiModule.WASI.defaultBindings,
+                    fs: wasmFs.fs,
+                  },
+                });
+
+                WebAssembly.instantiate(bytes, {
+                  ...wasi.getImports(bytes),
+                  ...new wasmImports.default().importObject,
+                }).then(async (vm) => {
+                  handler(vm);
+
+                  wasmFs.getStdOut().then((stdout) => console.log(stdout));
+                });
+              })
+        )
+    )
+  );
+
 global.openTeaVMWASMModule = (url, wasmRuntimeURL, handler) =>
   import(wasmRuntimeURL).then((wasmImports) => {
     let options = {};
@@ -82,19 +117,3 @@ global.openTeaVMWASMModule = (url, wasmRuntimeURL, handler) =>
         })
       );
   });
-
-global.openTinyGoWASMModule = (url, wasmRuntimeURL, handler) => {
-  import(wasmRuntimeURL).then((wasmImports) => {
-    const go = new wasmImports.default();
-
-    fetch(url)
-      .then((resp) => resp.arrayBuffer())
-      .then((bytes) =>
-        WebAssembly.instantiate(bytes, go.importObject).then(({ instance }) => {
-          go.run(instance);
-
-          handler(instance);
-        })
-      );
-  });
-};
