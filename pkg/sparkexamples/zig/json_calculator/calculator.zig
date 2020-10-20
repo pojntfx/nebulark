b54e6ca2407b68d24edcda36eefab37aad0cd6e0
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-20 23:00:12 +0000

    feat: Add JSON decoder for Zig JSON spark

--- pkg/sparkexamples/zig/json_calculator/calculator.zig
+++ pkg/sparkexamples/zig/json_calculator/calculator.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 
+const Input = struct {
+    firstAddend: i64,
+    secondAddend: i64
+};
+
+const Output = struct {
+    sum: i64
+};
+
 var input_encoded = std.ArrayList(u8).init(std.heap.page_allocator);
 
 export fn nebulark_ion_spark_construct() i32 {
@@ -23,6 +32,26 @@ export fn nebulark_ion_spark_input_set(index: i32 , input: u8 ) i32 {
 }
 
 export fn nebulark_ion_spark_open() i32 {
+    const right_bound = std.base64.standard_decoder.calcSize(input_encoded.items) catch |err| {
+        return 1;
+    };
+    var inputBuffer: [0x100]u8 = undefined;
+    var decoded_input = inputBuffer[0..right_bound];
+
+    std.base64.standard_decoder.decode(decoded_input, input_encoded.items) catch |err| {
+        return 1;
+    };
+
+    const input = std.json.parse(
+        Input, 
+        &std.json.TokenStream.init(decoded_input),
+        std.json.ParseOptions{
+            .allocator = std.heap.page_allocator
+        }
+    ) catch |err| {
+        return 1;
+    };
+
     return 0;
 }
 
