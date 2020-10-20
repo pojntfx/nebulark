7a6b21c5fc81a16f6acf6295c2e6bf80c3f2b82d
Felix Pojtinger <felix@pojtinger.com>
2020-10-20 22:37:24 +0000

    feat: Implement data input for Zig

--- pkg/sparkexamples/zig/json_calculator/calculator.zig
+++ pkg/sparkexamples/zig/json_calculator/calculator.zig
@@ -1,15 +1,25 @@
-const std = import("std");
+const std = @import("std");
+
+var input_encoded = std.ArrayList(u8).init(std.heap.page_allocator);
 
 export fn nebulark_ion_spark_construct() i32 {
     return 0;
 }
 
 export fn nebulark_ion_spark_input_set_length(length: i32) i32 {
+    input_encoded = std.ArrayList(u8).init(std.heap.page_allocator);
+
+    input_encoded.appendNTimes(0, @intCast(usize, length)) catch |err| {
+        return 1;
+    };
+
     return 0;
 }
 
 export fn nebulark_ion_spark_input_set(index: i32 , input: u8 ) i32 {
-    return index + 0;
+    input_encoded.items[@intCast(usize, index)] = input;
+
+    return 0;
 }
 
 export fn nebulark_ion_spark_open() i32 {
