6ef643b7d8173cc2d3b9be37154d2ab4a60b0cc7
Felix Pojtinger <felix@pojtinger.com>
2020-10-21 01:07:25 +0000

    refactor: Clean up package naming conventions and runtime confusion

---
+++ pkg/runtimes/teavm.go
@@ -0,0 +1,37 @@
+package runtimes
+
+import "syscall/js"
+
+type TeaVMRuntime struct {
+	wasmRuntimeURL string
+
+	*WASIRuntime
+}
+
+func NewTeaVMRuntime(wasmBinaryURL string, wasmRuntimeURL string) *TeaVMRuntime {
+	return &TeaVMRuntime{
+		wasmRuntimeURL: wasmRuntimeURL,
+
+		WASIRuntime: NewWASIRuntime(wasmBinaryURL),
+	}
+}
+
+func (s *TeaVMRuntime) LoadExports() error {
+	done := make(chan bool)
+
+	js.Global().Call("openTeaVMWASMModule", s.wasmBinaryURL, s.wasmRuntimeURL, js.FuncOf(func(_ js.Value, module []js.Value) interface{} {
+		s.wasmExports = module[0].Get("exports")
+
+		done <- true
+
+		return nil
+	}))
+
+	<-done
+
+	return nil
+}
+
+func (s *TeaVMRuntime) Run(input interface{}, output interface{}) error {
+	return s.WASIRuntime.Run(input, output, s.LoadExports)
+}
