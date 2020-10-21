15174746cac5f924d0beb4d865edc7860b560ad4
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-21 00:23:46 +0000

    refactor: Remove Go and JWebAssembly due to lack of sandbox capabilities

--- pkg/sparkexamples/go/simple_calculator/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/pojntfx/nebulark/pkg/sparkexamples/go/simple_calculator
-
-go 1.15
--- web/glue/registerFunctions.js
+++ web/glue/registerFunctions.js
@@ -1,31 +1,5 @@
 global.import = (...args) => import(args);
 
-global.openGoWASMModule = (url, handler) => {
-  const go = new Go();
-
-  fetch(url)
-    .then((resp) => resp.arrayBuffer())
-    .then((bytes) =>
-      WebAssembly.instantiate(bytes, go.importObject).then(({ instance }) => {
-        go.run(instance);
-
-        handler(instance);
-      })
-    );
-};
-
-global.openJWebAssemblyWASMModule = (url, wasmExecURL, handler) =>
-  import(wasmExecURL).then((wasmImports) =>
-    fetch(url)
-      .then((resp) => resp.arrayBuffer())
-      .then((bytes) =>
-        WebAssembly.instantiate(
-          bytes,
-          wasmImports.default
-        ).then(({ instance }) => handler(instance))
-      )
-  );
-
 global.openWASIWASMModule = (url, handler) =>
   import("https://unpkg.com/@wasmer/wasi@0.12.0/lib/index.esm.js").then(
     (wasiModule) =>
@@ -110,8 +84,6 @@ global.openTeaVMWASMModule = (url, wasmRuntimeURL, handler) =>
       .then((bytes) =>
         WebAssembly.instantiate(bytes, importObj).then((vm) => {
           importObj.teavm.logString.memory = vm.instance.exports.memory;
-
-          vm.instance.exports.main();
 
           handler(vm.instance);
         })
