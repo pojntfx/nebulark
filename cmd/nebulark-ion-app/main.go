9d0444cd1af5486c8635cb2812b40ca18b2546d5
Felix Pojtinger <felix@pojtinger.com>
2020-10-21 15:05:24 +0000

    feat: Add generic virtual machine for all runtimes

--- cmd/nebulark-ion-app/main.go
+++ cmd/nebulark-ion-app/main.go
@@ -3,58 +3,58 @@ package main
 import (
 	"github.com/maxence-charriere/go-app/v7/pkg/app"
 	"github.com/pojntfx/nebulark/pkg/components"
-	"github.com/pojntfx/nebulark/pkg/runtimes"
+	"github.com/pojntfx/nebulark/pkg/virtualmachines"
 )
 
 func main() {
 	app.Route("/", &components.AppComponent{
 		SimpleExamples: []components.CalculatorExample{
 			{
-				Title:       "C Calculator Simple Example",
-				WASIRuntime: runtimes.NewWASIRuntime("/web/examples/c/simple_calculator/main.wasm"),
+				Title:          "C Calculator Simple Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/c/simple_calculator/main.wasm").EnableWASIRuntime().Build(),
 			},
 			{
-				Title:       "C++ Calculator Simple Example",
-				WASIRuntime: runtimes.NewWASIRuntime("/web/examples/cpp/simple_calculator/main.wasm"),
+				Title:          "C++ Calculator Simple Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/cpp/simple_calculator/main.wasm").EnableWASIRuntime().Build(),
 			},
 			{
-				Title:       "AssemblyScript Calculator Simple Example",
-				WASIRuntime: runtimes.NewWASIRuntime("/web/examples/assemblyscript/simple_calculator/main.wasm"),
+				Title:          "AssemblyScript Calculator Simple Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/assemblyscript/simple_calculator/main.wasm").EnableWASIRuntime().Build(),
 			},
 			{
-				Title:       "Zig Calculator Simple Example",
-				WASIRuntime: runtimes.NewWASIRuntime("/web/examples/zig/simple_calculator/main.wasm"),
+				Title:          "Zig Calculator Simple Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/zig/simple_calculator/main.wasm").EnableWASIRuntime().Build(),
 			},
 			{
-				Title:         "TinyGo Calculator Simple Example",
-				TinyGoRuntime: runtimes.NewTinyGoRuntime("/web/examples/tinygo/simple_calculator/main.wasm", "/web/glue/tinygo/wasm_exec.js"),
+				Title:          "TinyGo Calculator Simple Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/tinygo/simple_calculator/main.wasm").EnableTinyGoRuntime("/web/glue/tinygo/wasm_exec.js").Build(),
 			},
 			{
-				Title:        "TeaVM Calculator Simple Example",
-				TeaVMRuntime: runtimes.NewTeaVMRuntime("/web/examples/teavm/simple_calculator/main.wasm", "/web/glue/teavm/wasm-runtime.js"),
+				Title:          "TeaVM Calculator Simple Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/teavm/simple_calculator/main.wasm").EnableTeaVMRuntime("/web/glue/teavm/wasm-runtime.js").Build(),
 			},
 		},
 
 		JSONExamples: []components.CalculatorExample{
 			{
-				Title:       "C Calculator JSON Example",
-				WASIRuntime: runtimes.NewWASIRuntime("/web/examples/c/json_calculator/main.wasm"),
+				Title:          "C Calculator JSON Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/c/json_calculator/main.wasm").EnableWASIRuntime().Build(),
 			},
 			{
-				Title:       "AssemblyScript Calculator JSON Example",
-				WASIRuntime: runtimes.NewWASIRuntime("/web/examples/assemblyscript/json_calculator/main.wasm"),
+				Title:          "AssemblyScript Calculator JSON Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/assemblyscript/json_calculator/main.wasm").EnableWASIRuntime().Build(),
 			},
 			{
-				Title:       "Zig Calculator JSON Example",
-				WASIRuntime: runtimes.NewWASIRuntime("/web/examples/zig/json_calculator/main.wasm"),
+				Title:          "Zig Calculator JSON Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/zig/json_calculator/main.wasm").EnableWASIRuntime().Build(),
 			},
 			{
-				Title:         "TinyGo Calculator JSON Example",
-				TinyGoRuntime: runtimes.NewTinyGoRuntime("/web/examples/tinygo/json_calculator/main.wasm", "/web/glue/tinygo/wasm_exec.js"),
+				Title:          "TinyGo Calculator JSON Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/tinygo/json_calculator/main.wasm").EnableTinyGoRuntime("/web/glue/tinygo/wasm_exec.js").Build(),
 			},
 			{
-				Title:        "TeaVM Calculator JSON Example",
-				TeaVMRuntime: runtimes.NewTeaVMRuntime("/web/examples/teavm/json_calculator/main.wasm", "/web/glue/teavm/wasm-runtime.js"),
+				Title:          "TeaVM Calculator JSON Example",
+				VirtualMachine: virtualmachines.NewWASMVirtualMachineBuilder().SetBinary("/web/examples/teavm/json_calculator/main.wasm").EnableTeaVMRuntime("/web/glue/teavm/wasm-runtime.js").Build(),
 			},
 		},
 	})
