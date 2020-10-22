7a9b6a3a33ee731d7625c5770bc259bf1803e4de
Jakob Waibel <jakob.waibel@web.de>
2020-10-22 13:42:35 +0200

    build: Include base64 library when build JSON C++ calculator

--- pkg/examples/cpp/json_calculator/calculator.cc
+++ pkg/examples/cpp/json_calculator/calculator.cc
@@ -1,7 +1,7 @@
-#include "base64/base64.h"
+#include "_deps/base64/base64.h"
+#include "build/_deps/json-src/single_include/nlohmann/json.hpp"
 #include <iostream>
 #include <vector>
-#include <nlohmann/json.hpp>
 
 using namespace std;
 using json = nlohmann::json;
@@ -15,101 +15,100 @@ int firstAddend;
 int secondAddend;
 int result;
 
-
-__attribute__((export_name("nebulark_ion_spark_construct")))
-int nebulark_ion_spark_construct() {
+__attribute__((export_name("nebulark_ion_spark_construct"))) int
+nebulark_ion_spark_construct() {
   return 0;
 }
 
-__attribute__((export_name("nebulark_ion_spark_input_set_length"))) 
-int nebulark_ion_spark_input_set_length(int length) { 
+__attribute__((export_name("nebulark_ion_spark_input_set_length"))) int
+nebulark_ion_spark_input_set_length(int length) {
   return 0;
 }
 
-__attribute__((export_name("nebulark_ion_spark_input_set")))
-int nebulark_ion_spark_input_set(int index, char input) {
-    inputEncoded.push_back(input);
+__attribute__((export_name("nebulark_ion_spark_input_set"))) int
+nebulark_ion_spark_input_set(int index, char input) {
+  inputEncoded.push_back(input);
   return 0;
 }
 
-__attribute__((export_name("nebulark_ion_spark_open"))) 
-int nebulark_ion_spark_open() {
+__attribute__((export_name("nebulark_ion_spark_open"))) int
+nebulark_ion_spark_open() {
   // untoggle comment again
   // string inputEncodedString = "";
   // for (int i = 0; i < sizeof(inputEncoded)/sizeof(inputEncoded[0]); i++) {
   //     inputEncodedString += string(1, inputEncoded[i]);
   // }
- //REMOVE LATER
- string inputEncodedString = "eyJmaXJzdEFkZGVuZCI6IDMsICJzZWNvbmRBZGRlbmQiOiAyfQ==";
- string inputDecodedString;
-  //std::vector<uint8_t> inputDecodedArray = base64::decode(inputEncodedString);
+  // REMOVE LATER
+  string inputEncodedString =
+      "eyJmaXJzdEFkZGVuZCI6IDMsICJzZWNvbmRBZGRlbmQiOiAyfQ==";
+  string inputDecodedString;
+  // std::vector<uint8_t> inputDecodedArray =
+  // base64::decode(inputEncodedString);
   bool decoder = Base64::Decode(inputEncodedString, &inputDecodedString);
 
   // string inputDecodedString = "";
-  // for (int h = 0; h < sizeof(inputDecodedArray)/sizeof(inputDecodedArray[0]); h++) {
-  //   inputDecodedString += string(1, inputDecodedArray[h]);      
+  // for (int h = 0; h < sizeof(inputDecodedArray)/sizeof(inputDecodedArray[0]);
+  // h++) {
+  //   inputDecodedString += string(1, inputDecodedArray[h]);
   // }
-  
+
   json j = json::parse(inputDecodedString);
   firstAddend = j.at("firstAddend");
   secondAddend = j.at("secondAddend");
   return 0;
 }
 
-__attribute__((export_name("nebulark_ion_spark_ignite")))
-int nebulark_ion_spark_ignite() {
-
-    result = firstAddend + secondAddend;
-    cout << result << endl;
-    //std::string s = j.dump();
+__attribute__((export_name("nebulark_ion_spark_ignite"))) int
+nebulark_ion_spark_ignite() {
+  result = firstAddend + secondAddend;
+  cout << result << endl;
+  // std::string s = j.dump();
 
   return 0;
 }
 
-__attribute__((export_name("nebulark_ion_spark_close")))
-int nebulark_ion_spark_close() {
- string outputDecodedString = string("{\"sum\": ") + std::to_string(result) + "}";
+__attribute__((export_name("nebulark_ion_spark_close"))) int
+nebulark_ion_spark_close() {
+  string outputDecodedString =
+      string("{\"sum\": ") + std::to_string(result) + "}";
 
-      // std::vector<uint8_t> outputDecodedArray;
+  // std::vector<uint8_t> outputDecodedArray;
 
-      // for (int i = 0; i < outputDecodedString.length(); i++) {
-      //     char currentChar = outputDecodedString.at(i);
-      //     outputDecodedArray.push_back(currentChar);
-      // }
+  // for (int i = 0; i < outputDecodedString.length(); i++) {
+  //     char currentChar = outputDecodedString.at(i);
+  //     outputDecodedArray.push_back(currentChar);
+  // }
 
-      string outputEncodedString;
-      bool encoder= Base64::Encode(outputDecodedString, &outputEncodedString);
+  string outputEncodedString;
+  bool encoder = Base64::Encode(outputDecodedString, &outputEncodedString);
 
-      std::vector<uint8_t> outputEncoded;
+  std::vector<uint8_t> outputEncoded;
 
-     for(int k = 0; k < outputEncodedString.length(); k++) {
-         char currentChar2 = outputEncodedString.at(k);
-         outputEncoded.push_back(currentChar2);
-     }
+  for (int k = 0; k < outputEncodedString.length(); k++) {
+    char currentChar2 = outputEncodedString.at(k);
+    outputEncoded.push_back(currentChar2);
+  }
   return 0;
 }
 
-__attribute__((export_name("nebulark_ion_spark_output_get_length")))
-int nebulark_ion_spark_output_get_length() {
-  
-  return sizeof(outputEncoded)/sizeof(outputEncoded[0]);
+__attribute__((export_name("nebulark_ion_spark_output_get_length"))) int
+nebulark_ion_spark_output_get_length() {
+  return sizeof(outputEncoded) / sizeof(outputEncoded[0]);
 };
 
-__attribute__((export_name("nebulark_ion_spark_output_get")))
-char nebulark_ion_spark_output_get(int index) {
- 
+__attribute__((export_name("nebulark_ion_spark_output_get"))) char
+nebulark_ion_spark_output_get(int index) {
   return outputEncoded[index];
 }
 
-__attribute__((export_name("nebulark_ion_spark_deconstruct")))
-int nebulark_ion_spark_deconstruct() {
-
+__attribute__((export_name("nebulark_ion_spark_deconstruct"))) int
+nebulark_ion_spark_deconstruct() {
   return 0;
 }
 
 int main() {
-    cout << "Hello World" << endl;
-    nebulark_ion_spark_open();
-    nebulark_ion_spark_ignite();
-    nebulark_ion_spark_close();
+  cout << "Hello World" << endl;
+  nebulark_ion_spark_open();
+  nebulark_ion_spark_ignite();
+  nebulark_ion_spark_close();
 }
