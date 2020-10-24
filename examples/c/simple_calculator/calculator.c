08e29aade5243f9541244dbb0c531b04366f7648
Felix Pojtinger <felix@pojtinger.com>
2020-10-24 19:40:32 +0000

    feat: Add simple C example

---
+++ examples/c/simple_calculator/calculator.c
@@ -0,0 +1,6 @@
+__attribute__((export_name("add"))) int add(int first_addend,
+                                            int second_addend) {
+  return first_addend + second_addend;
+}
+
+int main() { return 0; }
