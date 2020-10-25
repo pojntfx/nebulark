e0358b9cae431cd627c30e1353e11b807db510b7
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-10-25 18:20:09 +0000

    refactor: Eliminate all unsafe code

--- examples/rust/json_calculator/src/main.rs
+++ examples/rust/json_calculator/src/main.rs
@@ -1,6 +1,8 @@
 use base64;
+use once_cell::sync::Lazy;
 use serde;
 use serde_json;
+use std::sync::Mutex;
 
 fn main() {}
 
@@ -16,13 +18,27 @@ struct Output {
     sum: i32,
 }
 
-static mut INPUT_ENCODED: Vec<u8> = vec![];
-static mut OUTPUT_ENCODED: Vec<u8> = vec![];
+struct GlobalState {
+    input_encoded: Vec<u8>,
+    output_encoded: Vec<u8>,
 
-static mut FIRST_ADDEND: i32 = 0;
-static mut SECOND_ADDEND: i32 = 0;
+    first_addend: i32,
+    second_addend: i32,
 
-static mut SUM: i32 = 0;
+    sum: i32,
+}
+
+static GLOBAL_STATE: Lazy<Mutex<GlobalState>> = Lazy::new(|| {
+    Mutex::new(GlobalState {
+        input_encoded: vec![],
+        output_encoded: vec![],
+
+        first_addend: 0,
+        second_addend: 0,
+
+        sum: 0,
+    })
+});
 
 #[export_name = "nebulark_ion_spark_construct"]
 pub extern "C" fn construct() -> i32 {
@@ -31,30 +47,36 @@ pub extern "C" fn construct() -> i32 {
 
 #[export_name = "nebulark_ion_spark_input_set_length"]
 pub extern "C" fn input_set_length(length: i32) -> i32 {
-    unsafe {
-        INPUT_ENCODED = vec![0; length as usize];
-    }
+    let mut global_state = match GLOBAL_STATE.lock() {
+        Ok(s) => s,
+        Err(_) => return 1,
+    };
+
+    global_state.input_encoded = vec![0; length as usize];
 
     return 0;
 }
 
 #[export_name = "nebulark_ion_spark_input_set"]
 pub extern "C" fn input_set(index: i32, input: u8) -> i32 {
-    unsafe {
-        INPUT_ENCODED[index as usize] = input;
-    }
+    let mut global_state = match GLOBAL_STATE.lock() {
+        Ok(s) => s,
+        Err(_) => return 1,
+    };
+
+    global_state.input_encoded[index as usize] = input;
 
     return 0;
 }
 
 #[export_name = "nebulark_ion_spark_open"]
 pub extern "C" fn open() -> i32 {
-    let input_encoded;
-    unsafe {
-        input_encoded = &INPUT_ENCODED;
-    }
+    let mut global_state = match GLOBAL_STATE.lock() {
+        Ok(s) => s,
+        Err(_) => return 1,
+    };
 
-    let input_decoded = match base64::decode(input_encoded) {
+    let input_decoded = match base64::decode(&global_state.input_encoded) {
         Ok(i) => i,
         Err(_) => return 1,
     };
@@ -64,29 +86,34 @@ pub extern "C" fn open() -> i32 {
         Err(_) => return 1,
     };
 
-    unsafe {
-        FIRST_ADDEND = input.first_addend;
-        SECOND_ADDEND = input.second_addend;
-    }
+    global_state.first_addend = input.first_addend;
+    global_state.second_addend = input.second_addend;
 
     return 0;
 }
 
 #[export_name = "nebulark_ion_spark_ignite"]
 pub extern "C" fn ignite() -> i32 {
-    unsafe {
-        SUM = FIRST_ADDEND + SECOND_ADDEND;
-    }
+    let mut global_state = match GLOBAL_STATE.lock() {
+        Ok(s) => s,
+        Err(_) => return 1,
+    };
+
+    global_state.sum = global_state.first_addend + global_state.second_addend;
 
     return 0;
 }
 
 #[export_name = "nebulark_ion_spark_close"]
 pub extern "C" fn close() -> i32 {
-    let output;
-    unsafe {
-        output = Output { sum: SUM };
-    }
+    let mut global_state = match GLOBAL_STATE.lock() {
+        Ok(s) => s,
+        Err(_) => return 1,
+    };
+
+    let output = Output {
+        sum: global_state.sum,
+    };
 
     let output_decoded = match serde_json::to_vec::<Output>(&output) {
         Ok(o) => o,
@@ -95,29 +122,29 @@ pub extern "C" fn close() -> i32 {
 
     let output_encoded = base64::encode(output_decoded).into_bytes();
 
-    unsafe {
-        OUTPUT_ENCODED = output_encoded;
-    }
+    global_state.output_encoded = output_encoded;
 
     return 0;
 }
 
 #[export_name = "nebulark_ion_spark_output_get_length"]
 pub extern "C" fn output_get_length() -> i32 {
-    let output_encoded;
-    unsafe { output_encoded = &OUTPUT_ENCODED }
+    let global_state = match GLOBAL_STATE.lock() {
+        Ok(s) => s,
+        Err(_) => return 1,
+    };
 
-    return output_encoded.len() as i32;
+    return global_state.output_encoded.len() as i32;
 }
 
 #[export_name = "nebulark_ion_spark_output_get"]
 pub extern "C" fn output_get(index: i32) -> u8 {
-    let output_encoded;
-    unsafe {
-        output_encoded = &OUTPUT_ENCODED;
-    }
+    let global_state = match GLOBAL_STATE.lock() {
+        Ok(s) => s,
+        Err(_) => return 1,
+    };
 
-    return output_encoded[index as usize];
+    return global_state.output_encoded[index as usize];
 }
 
 #[export_name = "nebulark_ion_spark_deconstruct"]
