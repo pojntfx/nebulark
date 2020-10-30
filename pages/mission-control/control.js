a6babe572ca3b89874c08677a0a925677c10a081
Jakob Waibel <jakob.waibel@web.de>
2020-10-30 23:16:33 +0100

    feat: Add QRCode caption

--- pages/mission-control/control.js
+++ pages/mission-control/control.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-var QRCode = require('qrcode.react');
+var QRCode = require("qrcode.react");
 
 function Control() {
-
   const Body = styled.body`
-    font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
-    color: #404240; 
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    color: #404240;
   `;
 
   const Header = styled.h1`
@@ -17,24 +16,32 @@ function Control() {
     background-color: #404240;
     height: 5px;
     border: none;
-    border-radius: 2px;  
+    border-radius: 2px;
+  `;
+
+  const QRCodeCaption = styled.a`
+    color: #404240;
+    font-size: 12px;
   `;
 
   // This data will be fetched from an API later on
   const nebulaID = "woody-wood-wood";
-  const qrCode = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO";
+  const qrCode =
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO";
   const ions = "";
   const ionStatus = "";
 
   return (
     <>
-    <Body>
+      <Body>
         <Header> nebulark / {nebulaID} </Header>
         <Divider />
 
         <QRCode value={qrCode} />
-    </Body>
-      
+        <br></br>
+        <QRCodeCaption href="https://google.com">https://nebulark.spark/{nebulaID}</QRCodeCaption>
+        
+      </Body>
     </>
   );
 }
