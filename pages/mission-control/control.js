8e6281e6baa40406dcd34b1e721d0a09939c4738
Jakob Waibel <jakob.waibel@web.de>
2020-11-01 18:37:40 +0100

    refactor: Decluttering control.js

--- pages/mission-control/control.js
+++ pages/mission-control/control.js
@@ -2,11 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import Ion from "../../components/ion";
 import IPFS from "ipfs";
-
-var QRCode = require("qrcode.react");
+import QRCode from "qrcode.react";
 
 function Control() {
-
   const Body = styled.body`
     font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
     color: #404240;
@@ -40,73 +38,44 @@ function Control() {
     border-radius: 2px;
   `;
 
-  // This data will be fetched from an API later on
   const nebulaID = "woody-wood-wood";
   const qrCode =
     "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO";
   const ions = "";
   const ionStatus = "";
 
-  var uint8View = []
+  var uint8View = [];
+  var fileHash = null;
+
   function handleClick() {
-    console.error("Executing spark");
+    alert("Executing spark");
   }
 
   function captureFile(event) {
-    console.log("capture file...");
     event.preventDefault();
+
     const file = event.target.files[0];
     const reader = new window.FileReader();
+
     reader.readAsArrayBuffer(file);
     reader.onloadend = () => {
-
-      // setState is not defined
-      //setState({ buffer: Buffer(reader.result) });
-
       uint8View = new Uint8Array(reader.result);
-
-      console.log(uint8View);
-
-      //console.log(uint8View[0]);
     };
   }
 
-  async function onSubmit (event) {
-
+  async function onSubmit(event) {
     event.preventDefault();
-    console.log("on submit...");
 
-    const node = await IPFS.create()
-    const version = await node.version()
-
-    console.log('Version: ', version.version)
-
+    const node = await IPFS.create();
     const file = await node.add({
-      path: 'hello.txt',
-      content: uint8View
-    })
-    
-    console.log('Added file: ', file.path, file.cid.toString())
-    node.stop()
-    getResult()
-  }
+      path: "hello.txt",
+      content: uint8View,
+    });
 
+    fileHash = file.cid.toString();
+    console.log(fileHash)
 
-  // This works without changes 
-  async function getResult() {
-
-    const node = await IPFS.create()
-    
-    const stream = node.cat('QmdBQ2eBqvTtr714jygcGzhbvoMR96pch8NbJn1eSxmQNH')
-    let data = ''
-
-    for await (const chunk of stream) {
-
-      data += chunk.toString()
-    }
-
-    console.log(data)
-
+    node.stop();
   }
 
   return (
@@ -122,8 +91,8 @@ function Control() {
           https://nebulark.spark/{nebulaID}
         </QRCodeCaption>
         <Ion ionName="John's Phone" ionStatus={true} />
-        <Button onClick={handleClick}>Execute</Button>
 
+        <Button onClick={handleClick}>Execute</Button>
         <h1>Your Image</h1>
         <p>This image is stored on IPFS & The Etherium Blockchain!</p>
         <img src="" alt="" />
