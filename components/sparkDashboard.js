28831a920da6ed0b2d9bb649d3dd8e1f6ea541f1
Jakob Waibel <jakob.waibel@web.de>
2020-11-03 09:57:01 +0100

    feat: Remove "Submit" button of file uploader

--- components/sparkDashboard.js
+++ components/sparkDashboard.js
@@ -9,32 +9,30 @@ const Button = styled.button`
   border-radius: 2px;
 `;
 
-var uint8View = [];
 var fileHash = null;
 
 function SparkDashboard() {
   const [string, setString] = React.useState("");
 
-  function onSubmitJSON(event) {
-    event.preventDefault();
-    setString(new TextDecoder("utf-8").decode(uint8View));
-    console.log(string);
-    console.log("DONE");
-  }
-
   function handleClick() {
     alert("Executing spark");
   }
 
-  function captureFile(event) {
+  async function captureFile(event) {
     event.preventDefault();
 
     const file = event.target.files[0];
     const reader = new window.FileReader();
 
     reader.readAsArrayBuffer(file);
-    reader.onloadend = () => {
-      uint8View = new Uint8Array(reader.result);
+    reader.onloadend = async () => {
+      const node = await IPFS.create();
+      const file = await node.add({
+        path: "hello.txt",
+        content: new Uint8Array(reader.result),
+      });
+      fileHash = file.cid.toString();
+      console.log("https://ipfs.io/ipfs/" + fileHash);
     };
   }
 
@@ -50,40 +48,19 @@ function SparkDashboard() {
     };
   }
 
-  async function onSubmit(event) {
-    event.preventDefault();
-
-    const node = await IPFS.create();
-    const file = await node.add({
-      path: "hello.txt",
-      content: uint8View,
-    });
-    fileHash = file.cid.toString();
-    console.log("https://ipfs.io/ipfs/" + fileHash);
-  }
-  var test = "Hello";
   return (
     <>
-      <Button onClick={handleClick}>Execute</Button>
-      <img src="" alt="" />
-      <h2>Upload Image</h2>
-      <form onSubmit={onSubmit}>
-        <input type="file" onChange={captureFile} />
-        <input type="submit" />
-      </form>
-
-      {/* <form onSubmit={onSubmitJSON}>
-        <input type="file" onChange={captureFile} />
-        <input type="submit" />
-        <input type="text" value={string} onChange={(event) => setString(event.target.value)}></input>
-      </form> */}
-
+      <input type="file" onChange={(event) => captureFile(event)}></input>
+      <br />
       <input type="file" onChange={(event) => captureFileJSON(event)}></input>
       <input
         type="text"
         value={string}
         onChange={(event) => setString(event.target.value)}
       ></input>
+      <br />
+
+      <Button onClick={handleClick}>Execute</Button>
     </>
   );
 }
