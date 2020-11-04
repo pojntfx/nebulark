ffc8fa2b7b663c626c9c0da9b009d330cf82a46c
Jakob Waibel <jakob.waibel@web.de>
2020-11-04 18:24:24 +0100

    feat: Adding responsive design partly

--- components/ionDashboard.js
+++ components/ionDashboard.js
@@ -5,17 +5,11 @@ import QRCode from "qrcode.react";
 
 const QRCodeCaption = styled.a`
   color: #404240;
-  font-size: 12px;
+  font-size: 11px;
 `;
 
 const QRCodeStyled = styled(QRCode)`
   margin-top: 25px;
-  
-`;
-
-const Div = styled.div`
-  position: relative;
-  margin-left: 40px;
 `;
 
 const qrCode =
@@ -25,14 +19,14 @@ function IonDashboard({ nebulaID, ...otherProps }) {
   return (
     <>
       <div {...otherProps}>
-        <Div>
+       
           <QRCodeStyled size={190} value={qrCode} />
           <br></br>
           <QRCodeCaption href="https://google.com">
             https://nebulark.spark/{nebulaID}
           </QRCodeCaption>
           <Ion ionName="John's Phone" ionStatus={true} />
-        </Div>
+        
       </div>
     </>
   );
--- components/sparkDashboard.js
+++ components/sparkDashboard.js
@@ -3,43 +3,46 @@ import styled from "styled-components";
 import IPFS from "ipfs";
 
 const Button = styled.button`
-  background-color: green;
-  color: white;
-  border: none;
-  border-radius: 2px;
-  height: 30px;
-  width: 80px;
-  margin-left: -434px;
-  margin-top: 12px;
-`;
+    background-color: green;
+    color: white;
+    border: none;
+    border-radius: 2px;
+    height: 30px;
+    width: 80px;
+    margin-left: 346px;
+    margin-top: 12px;
+  `;
 
-const TextArea = styled.textarea`
-  width: 420px;
-  height: 200px;
-  margin-left: -780px;
-`;
+  const TextArea = styled.textarea`
+    width: 420px;
+    height: 200px;
+    margin-left: 0px;
+    resize: none;
+  `;
 
-const InputFile = styled.input`
-  margin: -100px;
-  margin-left: -780px;
-`;
+  const InputFile = styled.input`
+    margin: -100px;
+    margin-left: 0px;
+  `;
 
-const InputJSON = styled.input`
-  margin-left: -780px;
-  margin-bottom: 30px;
-`;
+  const InputJSON = styled.input`
+    margin-left: 0px;
+    margin-bottom: 30px;
+  `;
 
-const H1 = styled.h1`
-  margin-left: -780px;
-  margin-top: 50px;
-  margin-bottom: 30px;
-`;
+  const H1 = styled.h1`
+    margin-left: 0px;
+    margin-top: 50px;
+    margin-bottom: 30px;
+    font-size: 26px;
+  `;
 
-const Div = styled.div`
-  margin-top: -125px;
-`;
-
+  const Div = styled.div`
+    margin-top: -125px;
+  `;
+  
 function SparkDashboard({ ...otherProps }) {
+
   const [string, setString] = React.useState("");
   const [readerResult, setReaderResult] = React.useState();
 
@@ -79,12 +82,12 @@ function SparkDashboard({ ...otherProps }) {
   function captureFileJSON(event) {
     event.preventDefault();
 
-    const file = event.target.files[0];
-    const reader = new window.FileReader();
+    const fileJSON = event.target.files[0];
+    const readerJSON = new window.FileReader();
 
-    reader.readAsArrayBuffer(file);
-    reader.onloadend = () => {
-      setString(new TextDecoder().decode(new Uint8Array(reader.result)));
+    readerJSON.readAsArrayBuffer(fileJSON);
+    readerJSON.onloadend = () => {
+      setString(new TextDecoder().decode(new Uint8Array(readerJSON.result)));
     };
   }
 
