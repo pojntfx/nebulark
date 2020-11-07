e7d3fb3e2665872ccfc29cfea6fe6d38e182812f
Jakob Waibel <jakob.waibel@web.de>
2020-11-07 11:41:51 +0100

    refactor: Cleanup "runningSpark"

--- components/runningSpark.js
+++ components/runningSpark.js
@@ -1,22 +1,9 @@
 import React, { useRef, useState } from "react";
 import styled from "styled-components";
-//import Accordion from "./accordion";
 
 function RunningSpark() {
   const sparkName = "my-spark-1";
   const ionNumber = 4;
-  // Information on one spark (only running ones)
-  // if sparkstatus == running => display loading bar
-  // spark name
-  // number of ions calculating spark
-  // progess - either time since it started or "Waiting..."
-  // Red cancel button
-  // Divider below each row
-  // style hr
-
-  // [ ] if ionNumber bigger than 1 show Ions otherwhise show Ion
-  // [ ] Make timer working
-  // [ ] Implement loadingbar
 
   const Wrapper = styled.section`
     ul {
@@ -41,62 +28,32 @@ function RunningSpark() {
     border-radius: 2px;
   `;
 
-  const Accordion__content = styled.div`
-  background-color: white;
-  overflow: hidden;
-  transition: max-height 0.6s ease;
-`;
+  const AccordionWrapper = styled.section`
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+  `;
 
-const Accordion__text = styled.div`
-  position: relative;
-  font-weight: 400;
-  font-size: 14px;
-  padding: 18px;
-  height: 200px;
-`;
+  const Accordion__section = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
 
-const AccordionWrapper = styled.section`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-`;
+  const Accordion__button = styled.button`
+    background-color: red;
+    border-radius: 2px;
+    border: none;
+    width: 80px;
+    height: 20px;
+    color: white;
+  `;
 
-const Accordion__section = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+  const content = useRef(null);
 
-const Accordion__button = styled.button`
-  background-color: red;
-  border-radius: 2px;
-  border: none;
-  width: 80px;
-  height: 20px;
-  color: white;
-`;
-
-const [setActive, setActiveState] = useState("");
-const [setHeight, setHeightState] = useState("0px");
-const [setRotate, setRotateStatet] = useState("accordion_icon");
-
-const content = useRef(null);
-
-function toggleAccordion() {
-  setActiveState(setActive === "" ? "active" : "");
-  setHeightState(
-    setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
-  );
-
-  setRotateStatet(
-    setActive == "active" ? "accordion__icon" : "accordion_icon rotate"
-  );
-  console.log(content.current.scrollHeight);
-}
-  
   function cancelSpark(e) {
-    console.log("OK")
+    console.log("OK");
   }
 
   return (
@@ -108,18 +65,22 @@ function toggleAccordion() {
           <li>{ionNumber} Ions</li>
           <li>7 mins</li>
           <li>
-          <AccordionWrapper>
-      <Accordion__section>
-        <Accordion__button onClick={(e) => { if (window.confirm(`Are your sure you want to cancel spark ${sparkName}?`)) cancelSpark(e) } }>
-          Cancel
-        </Accordion__button>
-        {/* <Accordion__content ref={content} style={{ maxHeight: `${setHeight}` }}>
-          <Accordion__text
-            dangerouslySetInnerHTML={{ __html: props.content }}
-          />
-        </Accordion__content> */}
-      </Accordion__section>
-    </AccordionWrapper>  
+            <AccordionWrapper>
+              <Accordion__section>
+                <Accordion__button
+                  onClick={(e) => {
+                    if (
+                      window.confirm(
+                        `Are your sure you want to cancel spark ${sparkName}?`
+                      )
+                    )
+                      cancelSpark(e);
+                  }}
+                >
+                  Cancel
+                </Accordion__button>
+              </Accordion__section>
+            </AccordionWrapper>
           </li>
         </ul>
       </Wrapper>
