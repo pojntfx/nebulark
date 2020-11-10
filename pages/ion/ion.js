e5edbe8bbbfc27462565d77145572b54b2e59138
Jakob Waibel <jakob.waibel@web.de>
2020-11-10 19:27:26 +0100

    feat: Style "ion"

--- pages/ion/ion.js
+++ pages/ion/ion.js
@@ -1,49 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-
 const GridContainer = styled.div`
-    display: grid;
-    grid-template-rows: 260px 40px 40px;
-    grid-template-columns: 260px;
-    grid-auto-columns: minmax(120px, auto);
-    align-content: center;
-    justify-content: center;
-    row-gap: 10px;
-    
-    img {
-        width: 260px;
-        height: 260px;
-    }
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+  row-gap: 15px;
 
-    button {
-        width: 260px;
-        height: 40px;
-        border: none;
-        background-color: #2e87b0;
-    }
+  img {
+    width: 260px;
+    height: 260px;
+    margin-bottom: 20px;
+  }
 
-    input {
-        height: 40px;
-        width: 252px;
-    }
+  button {
+    color: white;
+    width: 260px;
+    height: 40px;
+    border: none;
+    background-color: #404040;
+    font-size: 24px;
+    border-radius: 2px;
+  }
 
-    
-    
+  input {
+    color: #404040;
+    height: 40px;
+    width: 252px;
+    radius: 2px;
+  }
 `;
+
 
 function Ion() {
 
+    const [string, setString] = useState();
 
-    return (
-        <>
-            <GridContainer>
-                <img src="/nebulark-logo.png"></img>
-                <button>New Nebula</button>
-                <input value="Got a key? Type it here!"></input>
-            </GridContainer>
-        </>
-    )
+  return (
+    <>
+      <GridContainer>
+        <img src="/nebulark-logo.png"></img>
+        <button>New Nebula</button>
+        <form><input defaultValue="Got a key? Type it here!" type="text" value={string} onChange={(event) => setString(event.target.value)} /></form>
+      </GridContainer>
+    </>
+  );
 }
 
 export default Ion;
