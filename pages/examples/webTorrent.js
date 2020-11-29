9e8975f0dde0af17ffbe3a44192e87e0a7874475
Jakob Waibel <jakob.waibel@web.de>
2020-11-29 22:28:10 +0100

    feat: Add webTorrent example skeleton

---
+++ pages/examples/webTorrent.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+    h1 {
+        color: red;
+    }
+`;
+
+function WebTorrentExample() {
+
+    return (
+        <Wrapper>
+            <h1>Hello World</h1>
+        </Wrapper>
+        
+    )
+}
+
+export default WebTorrentExample;
